723619e400cd0c237945b76bcc46e3e7d84526b0
Anderson Ono <andeerson.ono@gmail.com>
2021-03-18 21:46:31 -0300

    :tada: Challenge Finalized

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,18 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      if (Array.isArray(user_id)) {
+        return response.status(400).json({ error: "erro" });
+      }
+
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
